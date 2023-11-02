1edf0226e2cd5b62bb07c592d16daea1d91780a7
svetlanalok <sloktionova90@gmail.com>
2023-11-03 01:27:58 +0300

    new 1

---
+++ script.js
@@ -0,0 +1,59 @@
+// типы данных
+Number
+String
+null
+undefined
+Boolean
+BigInt = // 2*10^53-1
+Object = // может вкл.123456
+Symbol
+
+
+// обозначение переменных
+
+const x = "Николай"; // string
+function foo() {
+    var t = true;
+    let y = 6;
+}
+
+console.log(x)
+
+function jy() {
+
+}
+
+// математическик операции
+let a = 5 * 5;
+let b = (10 == '10'); // не строгое равенство/ прим. сравнивает число и строку// true
+let p = (10 === 9); // строгое равенство //folse
+
+=! // не равно
+
+
+// логические операции
+if(x < b){
+
+}
+else{
+
+}
+// switch(x) {
+//    case "valz"
+// }
+
+const array = [a, b, c, true] // одномерный массив
+ // многомерный
+
+
+ // цыклы
+ let j = 5
+ for(let i=0;i>j; i++) {
+    let count = 1
+ }
+
+ while (j > i) {
+    
+ }
+
+
